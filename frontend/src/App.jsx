eb371c0065f56268ac925c6c4e13c9ceef0247a4
devansh bhawsar <bhawsard75@gmail.com>
2025-01-26 22:00:21 +0530

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
 
 const socket = io("http://localhost:5000");
-
+// front start using run npm dev
 const App = () => {
   const [joined, setJoined] = useState(false);
   const [roomId, setRoomId] = useState("");
